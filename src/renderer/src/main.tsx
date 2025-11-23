a752609c63bc3582613e37b958bec4dbb0c0eaf1
bariseg <93949921+bariseg@users.noreply.github.com>
2025-11-24 01:29:29 +0300

    cesium build düzeltildi

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { SciChartSurface } from 'scichart'
 
+(window as any).CESIUM_BASE_URL = './cesium/'
+
 SciChartSurface.configure({
   wasmUrl: 'scichart2d.wasm'
 })
